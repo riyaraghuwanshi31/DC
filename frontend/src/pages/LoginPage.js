3991df637334e9156c14cbb04ec60b470044c46b
riyaR0831 <riyaraghuwanshi0831@gmail.com>
2026-04-17 17:08:00 +0530

    backend link updated

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -12,7 +12,7 @@ const LoginPage = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("https://dc-zbw5.onrender.com/api/auth/login", {
         email,
         password,
       });
