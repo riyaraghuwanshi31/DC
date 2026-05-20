96ec94297ed7935b12d65882033640204bcbf481
riyaR0831 <riyaraghuwanshi0831@gmail.com>
2026-05-21 02:18:12 +0530

    fix:correct CORS origins

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,12 @@ const app = express();
 
 // Allow all origins for testing (then restrict later)
 app.use(cors({
-  origin: ['https://dc-mu-five.vercel.app/', 'http://localhost:3000'],
+  origin: [
+  'https://dc-mu-five.vercel.app',
+  'https://dc-git-main-riya-raghuwanshis-projects.vercel.app', 
+  'https://dc-cry6jf77y-riya-raghuwanshis-projects.vercel.app',
+  'http://localhost:3000'
+],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
